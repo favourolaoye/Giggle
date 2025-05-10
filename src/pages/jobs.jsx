eb232c12b3361737627cf867fc2884ed062bf1ac
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2025-05-10 21:26:23 +0100

    skeleton simulation

--- src/pages/jobs.jsx
+++ src/pages/jobs.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Listing from "./listing";
 import Skeleton from "../components/skeleton";
-
 export default function Jobs() {
-    const [jobdata, setJobdata] = useState(true);
+    const [jobdata, setJobdata] = useState(false);
+
+    useEffect(() => {
+      setInterval(() => {
+        setJobdata(true)
+      }, 3000)
+    })
   return (
-    <div>
+      <div>
         
         {
             jobdata ? <Listing/> : <Skeleton/>
         }
     </div>
+    
+
   )
 }
